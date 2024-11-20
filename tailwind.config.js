0051ef6963b20851eeffbbb4cbae9eb888f713f6
azizca7 <ikromovazizbek256@gmail.com>
2024-11-20 12:00:37 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"], 
+  theme: {
+    extend: {
+     
+      screens: {
+        'custom': '1200px',
+      },
+    },
+  },
+  plugins: [],
+};
